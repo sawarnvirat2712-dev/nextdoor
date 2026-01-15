d52323a9bc6671d6e2d8e7b594582d370ad0fb69
sawarnvirat2712-dev <sawarnvirat2712@gmail.com>
2026-01-15 18:28:10 +0530

    login complete

--- services/views.py
+++ services/views.py
@@ -1,8 +1,9 @@
-
-
-# Create your views here.
 from django.shortcuts import render
 
+# Create your views here.
+from django.contrib.auth.decorators import login_required
 
+
+@login_required(login_url="/login/")
 def home(request):
-    return render(request, 'services/home.html')
+    return render(request, "services/home.html")
